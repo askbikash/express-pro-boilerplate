0e9db9f76dce14d039a13042f03b127fdbe6135c
Bikash Kumar <bikash.kumar@forwardcode.in>
2026-09-22 22:28:20 +0530

    feat: add User Mongoose model with password hashing and query helpers

--- src/models/User.mongo.js
+++ src/models/User.mongo.js
@@ -25,12 +25,11 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (this.isModified('password')) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
   }
-  next();
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
